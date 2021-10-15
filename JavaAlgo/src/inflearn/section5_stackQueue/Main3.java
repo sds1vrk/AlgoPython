2c0fc8cefbc30b09865d08b3e7000bcd8ac80b12
sds1vrk <sdsvrk@gmail.com>
2021-10-16 01:18:07 +0900

    queue stack 문제 풀이

--- JavaAlgo/src/inflearn/section5_stackQueue/Main3.java
+++ JavaAlgo/src/inflearn/section5_stackQueue/Main3.java
@@ -4,8 +4,47 @@ package inflearn.section5_stackQueue;
 import java.util.*;
 public class Main3 {
 
-    public int solution() {
+    public int solution(int n,int[][]arr,int m,int[]mArr) {
         int answer=0;
+
+        Stack<Integer>stack=new Stack<>();
+
+
+        for (int i:mArr) {
+            int k=i-1; // 행을 뽑는다.
+
+            int su=0;
+            for (int j=0;j<n;j++) {
+                if (arr[j][k]!=0) {
+                    su=arr[j][k];
+                    arr[j][k]=0;
+                    break;
+                }
+            }
+
+            // 뽑은거 저장
+            // stack이 비어잇으면 저장
+
+            if (su!=0) {
+
+                if (stack.isEmpty()) {
+                    stack.push(su);
+                }
+                else {
+                    // 비워있지 않고 같으면 pop
+                    if (stack.peek().equals(su)) {
+                        answer+=2;
+                        stack.pop();
+                    }else{ // 아니면 그냥 저장
+                        stack.push(su);
+                    }
+                }
+            }
+        }
+
+
+
+
         return answer;
 
     }
@@ -18,22 +57,30 @@ public class Main3 {
         int arr[][]=new int[n][n];
 
         // 배열에 넣기
-        for (int i=n-1;i>=0;i--) {
+        for (int i=0;i<n;i++) {
             for (int j=0;j<n;j++) { //4 3 2 1 0
                 arr[i][j]=scan.nextInt();
             }
         }
 
         // 확인
-        for (int i=0;i<n;i++) {
-            for (int j=0;j<n;j++) {
-                System.out.print(arr[i][j]+" ");
-            }
-            System.out.println();
+//        for (int i=0;i<n;i++) {
+//            for (int j=0;j<n;j++) {
+//                System.out.print(arr[i][j]+" ");
+//            }
+//            System.out.println();
+//        }
+
+        // 진행 횟수
+        int m=scan.nextInt();
+        int mArr[]=new int[m];
+
+        for (int i=0;i<m;i++) {
+            mArr[i]=scan.nextInt();
         }
 
 
-
+        System.out.println(main.solution(n,arr,m,mArr));
 
     }
 }
